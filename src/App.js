27261bdd0f90233ab558dc18f5530914ff8cde8b
Alex Barrientos <84935458+aBarrientosA@users.noreply.github.com>
2021-09-19 21:16:36 -0400

    adding state functionality (delete)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-
-import './App.css';
-import Header from './components/Header'
+import "./App.css";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from "react";
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Reminder to wash the towels.",
+      day: "September 19",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Reminder to drink more Mountain Dew.",
+      day: "September 20",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Reminder to dirnk less Mountain Dew on Tuesdays.",
+      day: "March 24",
+      reminder: true,
+    },
+  ]);
+  //
+  //Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
   return (
     <div className="container">
-      <Header/>
+      <Header />
+      <Tasks tasks={tasks} onDelete={deleteTask}/>
     </div>
   );
 }
-
 export default App;
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,20 @@
-import React from "react";
 
-function Button() {
+import PropTypes from 'prop-types'
+
+const Button = ({ color, text , onClick}) => {
     return (
-        <button className="btn">Add</button>
+        <Button style={{ backgroundColor: color }} className='btn'
+        onClick = {onClick}>
+            {text}
+        </Button>
+    )
 }
-
+Button.defaultProps = {
+    color: 'steelBlue'
+}
+Button.defaultProps = {
+    text: PropTypes.string,
+    color: PropTypes.string,
+    onClick: PropTypes.func,
+}
 export default Button;
